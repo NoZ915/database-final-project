7a369cbc5e213fe9a93b0e2dbf68289bb9c6c8ea
NoZ915 <tina0915tw0915@gmail.com>
2024-01-18 15:13:30 +0800

    added: signup or login first

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   if (!req.session.isLoggedIn) {
     // return res.redirect("/login");
-    req.flash("error", "請先登入");
+    req.flash("error", "請先登入或註冊");
         // return res.redirect("/login");
         return req.session.save(err => {
           res.redirect('/login');
